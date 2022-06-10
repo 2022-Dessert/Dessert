e5d15ad1ff0047e9515c8447f90cfc49f70ad948
KimSeongYeon <syg01103@naver.com>
2022-06-10 13:33:31 +0900

    francise 재업

--- static/js/login.js
+++ static/js/login.js
@@ -2,3 +2,15 @@
 // loginClick.addEventListener("click", (event) => {
 //   location.href = "mainPage.html";
 // });
+
+function loginchecking() {
+  let id = document.getElementById("loginid");
+  let pw = document.getElementById("loginpw");
+
+  if (id.value === "dessert" && pw.value === "seong") {
+    alert("반갑습니다!");
+    window.location.href = "header.html";
+  } else {
+    alert("다시 시도해주세요!");
+  }
+}
